285b982c7d151de7ab1d35dbb78110e48243f0c3
David Jeong <drumrobot@1do.space>
2022-06-01 18:57:11 +0900

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueAmchart4Sample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueAmchart4Sample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-amchart4-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueAmchart4Sample } from './vue-amchart4-sample.vue';
